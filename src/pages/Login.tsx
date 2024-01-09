80d1c7f63d701898a415298e4c33657603973d67
danilo silva <ds5695749@gmail.com>
2024-01-09 11:34:56 -0300

    ADD campos email e senha

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -24,94 +24,33 @@ export default function Login({ navigation }) {
     if(ValueEmail == Login.GetValue().email && ValueSenha == Login.GetValue().senha){
       navigation.navigate("Inicio")
     }else{
-      SetStatus(true)
+      
     }    
   }
   
   
-  const [statusErro,SetStatus]= useState(false)
-
-  const StatusErro = () =>{
-    return(
-        <View style={styles.con}>
-            <Text>Email ou senha incorretos!</Text>
-            <View style={styles.container_button}>
-            <Button_component title="OK" onPress={()=>{SetStatus(false)}} altura={44} largura={249}/>  
-            </View>
-        </View>
-    )
-}
-
-
 
   useEffect(()=>{
-    statusErro
+    
   })
 
   return (
     <View style={styles.container}>
-      {statusErro == true ? <StatusErro/> : "" }
-      <View style={styles.titulos}>
-        <Text style={styles.text1}>Que bom ter você por aqui</Text>
-        <Text style={styles.text2}>preencha os dados para continuar</Text>
-      </View>
+        <View style={styles.boxInputs}>
+            <View style={styles.inputs}>
+            <Text style={styles.text}>Email</Text>
+            <TextInput style={styles.textInpt} placeholder="digite seu email" onChangeText={SetValueEmail}></TextInput>
+            </View>
+            
+            <View style={styles.inputs}>
+            <Text style={styles.text}>Senha</Text>
+            <TextInput style={styles.textInpt} placeholder="digite sua senha" onChangeText={SetValueSenha}></TextInput>
+            </View>
 
-      <View>
-        <Text style={styles.email}>Email</Text>
-        <TextInput
-          placeholder="digite seu email"
-          style={styles.inputs1}
-          onChangeText={SetValueEmail}
-          value={ValueEmail}
-          keyboardType="email-address"
-          
-        ></TextInput>
-      </View>
-
-      <View>
-        <Text style={styles.senha}>Senha</Text>
-
-        <View style={styles.inputs}>
-        <TextInput
-          placeholder="digite sua senha"
-          style={styles.inputs2}
-          onChangeText={SetValueSenha}
-          value={ValueSenha}
-          secureTextEntry= {visibility}
-        ></TextInput>
-        <TouchableOpacity onPress={mudarVisibilidade}>
-          <Ionicons name="eye" color="#C5D0E6" size={25} />
-        </TouchableOpacity>
+            <View style={styles.button}>
+                <Text style={{color: "#fff"}}>Entrar</Text>
+            </View>
         </View>
-      </View>
-
-      <View style={styles.box}>
-      
-        <Button_component title="Entrar" onPress={Verificacao} altura={44} largura={336}/>
-        <View style={styles.esqueciMinhaSenha}>
-          <Text style={styles.esqueciMinhaSenha}>Esqueceu sua senha? clique aqui</Text>
-        </View>
-      </View>
-
-      <View style={styles.boxLoginAlternativo}>
-        <View style={styles.linha} />
-        <View style={styles.caixaTexto}>
-          <Text style={styles.loginAlternativo}>ou faça login com </Text>
-        </View>
-        <View style={styles.linha} />
-      </View>
-
-      <View style={styles.containerImg}>
-        <Image source={require("../Assets/google.png")} style={styles.Image} />
-        <Image source={require("../Assets/instagram.png")} style={styles.Image} />
-        <Image source={require("../Assets/facebook.png")}style={styles.Image}/>
-        <Image source={require("../Assets/linkedin.png")} style={styles.Image} />
-      </View>
-
-      <View style={styles.container_textos}>
-        <Text style={styles.colorText}>Não tem uma conta?</Text>
-        <Text style={styles.colorText}> Cadastre-se agora!</Text>
-      </View>
     </View>
   );
 }
--- src/styles/LoginStyles.tsx
+++ src/styles/LoginStyles.tsx
@@ -6,7 +6,7 @@ export const styles = StyleSheet.create({
   
     container: {
         flex: 1,
-        backgroundColor: "#fff",
+        backgroundColor: "#3EA2D0",
         color: "black",
         display: "flex",
         alignItems: "center",
@@ -14,143 +14,47 @@ export const styles = StyleSheet.create({
         gap: 20,
         flexDirection: "column",
       },
-      containerImg: {
-        display: "flex",
-        flexDirection: "row",
-        gap: 10,
-      },
-      box: {
-        display: "flex",
-        width: "100%",
-        alignItems: "center",
-        position: "relative",
-      },
-      text1: {
-        color: "#003778",
-        fontSize: 22,
-        marginLeft: 19,
-        
-      },
-      text2: {
-        color: "#003778",
-        fontSize: 12,
-        marginLeft: 19,
-      },
-      textBtn: {
-        color: "#fff",
-      },
-      email: {
-        fontSize: 16,
-        color: "#003778",
-        marginBottom: 16,
-      },
-      senha: {
-        fontSize: 16,
-        color: "#003778",
-        marginBottom: 16,
-      },
-      button: {
-        height: 50,
-        width: 336,
-        backgroundColor: "#3EA2D0",
-        borderRadius: 8,
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      },
-      esqueciMinhaSenha: {
-        fontSize: 12,
-        start: 30,
-        marginTop: 15,
-        color: "#003778"
-      },
-      container_button:{
-        position: "absolute",
-        bottom: 30
-      },
-      container_textos: {
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "row",
-      },
-      colorText: {
-        color: "#3E3E3E",
-        fontSize: 12,
-      },
-      Image: {
-        height: 30,
-        width: 30,
-      },
-      titulos: {
-        end: 40,
-      },
-      con:{
-        height: 297,
-        width: 332,
+      
+    boxInputs : {
+        height: 350,
+        width: 340,
         backgroundColor: "#fff",
-        borderStyle: "solid",
-        borderWidth: 1,
-        borderColor: "#C5D0E6",
-        position: "absolute",
-        zIndex:20,
+        borderRadius: 25,
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
-    },
-    btn:{
-        backgroundColor: "#3EA2D0",
-        height: 44,
-        width: 249,
-        borderRadius: 8,
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center"
-    },
-    text:{
-        color: "#fff"
-    },inputs1:{
-        backgroundColor: "#fff",
-        height: 46,
-        width: 336,
-        borderRadius: 8,
-        
-        borderStyle: "solid",
-        borderWidth: 1,
-        borderColor: "#C5D0E6",
-        padding: 10
-    },
-    inputs2:{
-      width: 290,
-      height: 50,
-      borderRadius: 8,
+        flexDirection: "column",
+        gap:30,
+        position: "relative"
     },
     inputs:{
+      height: 50,
+      width: 300,
+      backgroundColor : "#fff",
+      border: "1px solid #3EA2D0",
+      borderRadius: 10,
+      position: "relative" 
+    },
+    text: {
+      position: "absolute",
+      left: 0,
+      top: -20,
+      color: "#3EA2D0"
+    },
+    textInpt: {
+      height: 50,
+      width: 300,
+      borderRadius: 10,
+      padding: 10,
+      color: "grey"
+    },
+    button: {
+      height: 50,
+      width: 300,
+      backgroundColor: "#3EA2D0",
+      borderRadius: 10,
       display: "flex",
-      flexDirection: "row",
-      backgroundColor: "#fff",
+      justifyContent: "center",
       alignItems: "center",
-      height: 46,
-      width: 336,
-      borderRadius: 8,
-      borderStyle: "solid",
-      borderWidth: 1,
-      borderColor: "#C5D0E6",
-      padding: 10
-    },
-    texts:{
-        fontSize: 30
-    },
-    caixaTexto:{
-      display: "flex", flexDirection: "row"
-    },
-    linha:{
-      width:105, height: 1, backgroundColor: "#C5D0E6" 
-    },
-    loginAlternativo:{
-      width: 150, textAlign: "center",color: "#2A3563" 
-    },
-    boxLoginAlternativo:{
-      flexDirection: "row", alignItems: "center", marginTop: 50 
     }
 })
